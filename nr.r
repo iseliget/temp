2054829ea72587f6c5be8932b0dafd1aaafd4f2f
ut07x2a <chaoguo94@gmail.com>
2017-09-19 20:00:47 -0400

    Update nr.r

--- nr.r
+++ nr.r
@@ -13,3 +13,34 @@ gradient = function(X,Y,coef) {
 
 	return (result)
 }
+
+Hessian_first = function(X,Y,coeff) {
+	result = matrix(0, nrow=ncol(X), ncol=ncol(X))
+
+	for (i in 1:nrow(X)) {
+		eta = X[i,,drop=FALSE] %*% coeff
+
+		const_term_num = (-1)*((dnorm(eta))^2)
+		const_term_don = pnorm(eta)*(1-pnorm(eta))
+		const_term = as.numeric(const_term_num/const_term_don)
+
+		result = result + const_term * (t(X[i,,drop=FALSE]) %*% X[i,,drop=FALSE])
+	}
+
+	print("Hessian matrix:")
+	print(result)
+	return (result)
+}
+
+log_likelihood = function(X,Y,coeff) {
+	result = 0
+
+	for (i in 1:nrow(X)) {
+		first_const = as.numeric(Y[i,1,drop=FALSE]) * log(mu(X[i,,drop=FALSE], coeff)/(1 - mu(X[i,,drop=FALSE], coeff)))
+		second_const = log(1 - mu(X[i,,drop=FALSE], coeff))
+
+		result = result + first_const + second_const
+	}
+
+	return (result)
+}
