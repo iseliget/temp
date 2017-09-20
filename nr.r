123a2dd8c2c80e20c2a9f1dcb9f4d8386a927fd6
ut07x2a <chaoguo94@gmail.com>
2017-09-20 13:44:10 -0400

    Update nr.r
    造呀么造轮子

--- nr.r
+++ nr.r
@@ -1,14 +1,44 @@
-gradient = function(X,Y,coef) {
+library(MASS)
+attach(birthwt)
+
+# data preprocessings
+birthwt$race <- factor(birthwt$race, label=c("white", "black", "other"))
+birthwt$ptd <- factor(birthwt$ptl>0)
+birthwt$ftv <- factor(birthwt$ftv)
+
+levels(birthwt$ftv)[-(1:2)] <- "2+"
+
+# setting up design matrix and response matrix
+X<-model.matrix(~age + lwt + race + smoke + ptd + ht + ui + ftv, data=birthwt[c("age","lwt","race","smoke","ptd","ht","ui","ftv")])
+Y<-as.matrix(birthwt$low)
+
+# user-defined functions
+eta = function(x, coeff) {
+	return (x %*% coeff)
+}
+
+mu = function(x, link_func=default_link) {
+	return (default_link(x))
+}
+
+default_link = function(x) {
+	return (pnorm(x))
+}
+
+dnorm_deriv = function(x) {
+	result = (-x)/(sqrt(2*pi)) * dnorm(x)
+	return (result)
+}
+
+gradient = function(X,Y,coeff) {
 	result = matrix(0, nrow=ncol(X), ncol=1)
 
 	for (i in 1:nrow(X)) {
+		first_const = as.numeric(Y[i,1]) - mu(eta(X[i,,drop=FALSE], coeff))
+		second_const = 1/(mu(eta(X[i,,drop=FALSE], coeff))*(1 - mu(eta(X[i,,drop=FALSE], coeff))))
+		third_const = dnorm(eta(X[i,,drop=FALSE], coeff))[1,1]
 
-		first_const = as.numeric(Y[i,1]) - mu(X[i,,drop=FALSE], coef)
-		second_const = 1/(mu(X[i,,drop=FALSE], coef)*(1 - mu(X[i,,drop=FALSE], coef)))
-		third_const = pnorm(X[i,,drop=FALSE] %*% coef)[1,1]
-
-		result = result + (first_const*second_const*third_const)*t(X[i,,drop=FALSE])
-
+		result = result + (as.numeric(first_const*second_const*third_const))*t(X[i,,drop=FALSE])
 	}
 
 	return (result)
@@ -18,7 +48,7 @@ Hessian_first = function(X,Y,coeff) {
 	result = matrix(0, nrow=ncol(X), ncol=ncol(X))
 
 	for (i in 1:nrow(X)) {
-		eta = X[i,,drop=FALSE] %*% coeff
+		eta = eta(X[i,,drop=FALSE], coeff)
 
 		const_term_num = (-1)*((dnorm(eta))^2)
 		const_term_don = pnorm(eta)*(1-pnorm(eta))
@@ -27,8 +57,21 @@ Hessian_first = function(X,Y,coeff) {
 		result = result + const_term * (t(X[i,,drop=FALSE]) %*% X[i,,drop=FALSE])
 	}
 
-	print("Hessian matrix:")
-	print(result)
+	return (result)
+}
+
+Hessian_second = function(X,Y,coeff) {
+	result = matrix(0, nrow=ncol(X), ncol=ncol(X))
+
+	for (i in 1:nrow(X)) {
+		eta = eta(X[i,,drop=FALSE], coeff)
+
+		first_const = as.numeric(((Y[i,1]-pnorm(eta))*dnorm_deriv(eta))/(pnorm(eta)*(1-pnorm(eta))))
+		second_const = as.numeric(((Y[i,1]-pnorm(eta))*(2*mu(eta)*dnorm(eta)^2 - dnorm(eta)^2))/(pnorm(eta)^2 * (1-pnorm(eta))^2))
+		
+		result = result + (first_const + second_const) * (t(X[i,,drop=FALSE]) %*% X[i,,drop=FALSE])
+	}
+
 	return (result)
 }
 
@@ -44,3 +87,38 @@ log_likelihood = function(X,Y,coeff) {
 
 	return (result)
 }
+
+Fisher_scoring = function(X,Y) {
+	coeff = matrix(0, nrow=ncol(X), ncol=1)
+
+	for (i in 1:10) {
+		print(paste("=========================Fisher Scoring ITERATION ", i , "======================================="))
+		coeff = coeff - solve(Hessian_first(X,Y,coeff)) %*% gradient(X,Y,coeff)
+		print("coef after iteration")
+		print(coeff)
+		print("log-likelikhood after iteration")
+		print(log_likelihood(X,Y,coeff))
+	}
+
+	return (coeff)
+}
+
+Newton_rapshon = function(X,Y) {
+	coeff = matrix(0, nrow=ncol(X), ncol=1)
+
+	for (i in 1:10) {
+		print(paste("=========================Newton Rapshon ITERATION ", i , "======================================="))
+		coeff = coeff - solve(Hessian_first(X,Y,coeff)+Hessian_second(X,Y,coeff)) %*% gradient(X,Y,coeff)
+		print("coef after iteration")
+		print(coeff)
+		print("log-likelikhood after iteration")
+		print(log_likelihood(X,Y,coeff))
+	}
+
+	return (coeff)
+}
+
+Fisher_scoring(X,Y)
+Newton_rapshon(X,Y)
+
+rm(list = ls())
