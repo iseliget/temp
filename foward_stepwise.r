d5805398c047a46720888d8b3870777db237f334
ut07x2a <chaoguo94@gmail.com>
2017-11-15 15:46:17 -0500

    Update foward_stepwise.r

--- foward_stepwise.r
+++ foward_stepwise.r
@@ -1,46 +1,104 @@
-Sys.setenv(LANG = "en")
 library(dplyr)
 
-stepwise_forward = function(data_object, response_var, included_covar) {
-	# selection based on p-value will stop if p-value is not significant
-
-	all_covariates = setdiff(colnames(data_object), response_var)
+stepwise_forward_AIC = function(data_object, response, included_covar) {
+	all_covar = setdiff(colnames(data_object), response)
 	included_covar = included_covar
-	candidate_covar = setdiff(all_covariates, included_covar)
+	candidate_covar = setdiff(all_covar, included_covar)
 
-	for (i in 1:20) {
-		smallest_p = 1
+	reg_formula = paste(response, "~", paste(included_covar, collapse=" + "))
+	base_model_obj = do.call("lm", list(as.formula(reg_formula), data=as.name("data_object")))
+	base_AIC = AIC(base_model_obj)
 
+	for (i in 1:length(candidate_covar)) {
+		print(paste("Iteration ", i))
+
+		improvement = c()
 		for (covar in candidate_covar) {
 			current_covar = c(included_covar,covar)
-			rg_formula = paste(response_var, "~", paste(current_covar, collapse=" + "))
-			model_obj = do.call("lm", list(as.formula(rg_formula), data=as.name("data_object")))
-			coef = summary(model_obj)$coefficients
-			current_p = coef[covar,4]
 
-			if (current_p <= smallest_p) {
-				smallest_p = current_p
-				cand = covar
-			}
+			reg_formula = paste(response, "~", paste(current_covar, collapse=" + "))
+			model_obj = do.call("lm", list(as.formula(reg_formula), data=as.name("data_object")))
+			current_AIC = AIC(model_obj)
+
+			improvement = c(improvement, base_AIC-current_AIC)
 		}
 
-		if (smallest_p  > 0.05) {
-			print(paste("FINAL MODEL: contains ", paste(included_covar, collapse=", ")))
-			print("FINAL MODE SUMMARY: ")
-
-			formula = paste(response_var, "~", paste(included_covar, collapse=" + "))
-			final_model_obj = do.call("lm", list(as.formula(formula), data=as.name("data_object")))
-			print(summary(final_model_obj))
-			break		
+		if (min(improvement) <= 0) {
+			break
 		} else {
-			included_covar = c(included_covar, cand)
-			print(paste("STEP ",i, ": model contains ", paste(included_covar, collapse=", ")))
-			candidate_covar = setdiff(candidate_covar, cand)
+			names(improvement) = candidate_covar
+			cand = names(improvement)[which.max(improvement)]
+			candidate_covar = setdiff(candidate_covar,cand)
+			included_covar = c(included_covar,cand)
 		}
+		print(paste("AIC: ", base_AIC-max(improvement)))
+
+		# update base AIC
+		reg_formula = paste(response, "~", paste(included_covar, collapse=" + "))
+		model_obj = do.call("lm", list(as.formula(reg_formula), data=as.name("data_object")))
+		base_AIC = AIC(model_obj)
+
 	}
 
+	print(summary(model_obj))
 }
 
 parkinsons = read.csv("parkinsons.csv", header=TRUE, sep=",")
 parkinsons = dplyr::select(parkinsons, -motor_UPDRS)
-stepwise_forward(data_object=parkinsons, response_var="total_UPDRS", included_covar=c("sex","age","test_time"))
+
+stepwise_forward_AIC(data_object=parkinsons, 
+	response="total_UPDRS", 
+	included_covar=c("sex","age","test_time"))
+
+library(dplyr)
+
+stepwise_forward_BIC = function(data_object, response, included_covar) {
+	all_covar = setdiff(colnames(data_object), response)
+	included_covar = included_covar
+	candidate_covar = setdiff(all_covar, included_covar)
+
+	reg_formula = paste(response, "~", paste(included_covar, collapse=" + "))
+	base_model_obj = do.call("lm", list(as.formula(reg_formula), data=as.name("data_object")))
+	base_BIC = BIC(base_model_obj)
+
+	for (i in 1:length(candidate_covar)) {
+		print(paste("Iteration ", i))
+
+		improvement = c()
+		for (covar in candidate_covar) {
+			current_covar = c(included_covar,covar)
+
+			reg_formula = paste(response, "~", paste(current_covar, collapse=" + "))
+			model_obj = do.call("lm", list(as.formula(reg_formula), data=as.name("data_object")))
+			current_BIC = BIC(model_obj)
+
+			improvement = c(improvement, base_BIC-current_BIC)
+		}
+
+		if (min(improvement) <= 0) {
+			break
+		} else {
+			names(improvement) = candidate_covar
+			cand = names(improvement)[which.max(improvement)]
+			candidate_covar = setdiff(candidate_covar,cand)
+			included_covar = c(included_covar,cand)
+		}
+		print(paste("BIC: ", base_BIC-max(improvement)))
+
+		# update base BIC
+		reg_formula = paste(response, "~", paste(included_covar, collapse=" + "))
+		model_obj = do.call("lm", list(as.formula(reg_formula), data=as.name("data_object")))
+		base_BIC = BIC(model_obj)
+
+	}
+
+	print(summary(model_obj))
+}
+
+parkinsons = read.csv("parkinsons.csv", header=TRUE, sep=",")
+parkinsons = dplyr::select(parkinsons, -motor_UPDRS)
+
+stepwise_forward_BIC(data_object=parkinsons, 
+	response="total_UPDRS", 
+	included_covar=c("sex","age","test_time"))
+
